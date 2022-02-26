6a8e9e40a9aa9b733edcfed555ff902a1ed83875
Chisan Ahn <chisan2001@gmail.com>
2022-02-27 02:00:25 +0900

    refact: Promise 이용해서 리팩토링

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ let isTimerRunning = false;
 let startTime;
 let setCount = 1;
 
-let setGoal = 5;
-let setIntervalTime = 30;
-let exerciseIntervalTime = 1 * 60 + 30;
+let setGoal = 3;
+let setIntervalTime = 3;
+let exerciseIntervalTime = 5;
 let exerciseName = "새로운 운동";
 
 function secondToString(second) {
@@ -22,54 +22,55 @@ function secondToString(second) {
 }
 
 function startTimer(timerSecond) {
-	nextBtn.blur();
-	stopBtn.style.display = "block";
-	nextBtn.style.display = "none";
-	isTimerRunning = true;
-	startTime = Date.now();
+	return new Promise((resolve, reject) => {
+		stopBtn.style.display = "block";
+		nextBtn.style.display = "none";
+		isTimerRunning = true;
+		startTime = Date.now();
 
-	const runTimer = setInterval(() => {
-		let remainSecond = (timerSecond - (Date.now() - startTime) / 1000).toFixed(0);
-		console.log(remainSecond);
-		timerDisplay.textContent = secondToString(remainSecond);
-		if (remainSecond <= 0) {
-			alarm.play();
+		const runTimer = setInterval(() => {
+			let remainSecond = (timerSecond - (Date.now() - startTime) / 1000).toFixed(0);
+			timerDisplay.textContent = secondToString(remainSecond);
+			if (remainSecond <= 0) {
+				stopTimer();
+			}
+		}, 1000);
+
+		stopBtn.addEventListener("click", (event) => {
+			stopBtn.blur();
 			stopTimer();
-			alert("휴식 끝!");
-		}
-	}, 1000);
+		});
 
-	stopBtn.addEventListener("click", (event) => {
-		stopBtn.blur();
-		stopTimer();
+		const stopTimer = function () {
+			clearInterval(runTimer);
+			alarm.play();
+			stopBtn.style.display = "none";
+			nextBtn.style.display = "block";
+			isTimerRunning = false;
+			timerDisplay.textContent = "00:00";
+			resolve();
+		};
 	});
-
-	const stopTimer = function () {
-		stopBtn.style.display = "none";
-		nextBtn.style.display = "block";
-		isTimerRunning = false;
-		clearInterval(runTimer);
-		if (setCount === setGoal) {
-			setCount = 1;
-			mainText.textContent = mainText.textContent = `${exerciseName} 시작!`;
-		} else {
-			setCount++;
-			mainText.textContent = `${exerciseName} ${setCount} 세트 시작`;
-		}
-		timerDisplay.textContent = "00:00";
-	};
 }
 
 // 마지막 세트 완료
 const startNewExercise = function () {
 	mainText.textContent = `${exerciseName} ${setCount} 세트 완료`;
-	startTimer(exerciseIntervalTime);
+	startTimer(exerciseIntervalTime).then(() => {
+		setCount = 1;
+		mainText.textContent = mainText.textContent = `${exerciseName} 시작!`;
+		alert("휴식 완료!");
+	});
 };
 
 // 세트 완료
 const finishSet = function () {
 	mainText.textContent = `${exerciseName} ${setCount} 세트 완료`;
-	startTimer(setIntervalTime);
+	startTimer(setIntervalTime).then(() => {
+		setCount++;
+		mainText.textContent = `${exerciseName} ${setCount} 세트 시작`;
+		alert("휴식 완료!");
+	});
 };
 
 document.addEventListener("keydown", (event) => {
@@ -84,6 +85,7 @@ document.addEventListener("keydown", (event) => {
 });
 
 nextBtn.addEventListener("click", (event) => {
+	nextBtn.blur();
 	if (isTimerRunning) return;
 	if (setCount === setGoal) {
 		startNewExercise();
